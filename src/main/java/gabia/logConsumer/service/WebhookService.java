b53344c2082f01684d44eb2105231622751fa539
Lucas(주영준) <jyj@gabia.com>
2021-07-27 10:52:29 +0900

    feat: webhook message 생성 factory 적용

--- src/main/java/gabia/logConsumer/service/WebhookService.java
+++ src/main/java/gabia/logConsumer/service/WebhookService.java
@@ -13,6 +13,7 @@ import gabia.logConsumer.dto.SlackDTO.Attachment;
 import gabia.logConsumer.dto.WebhookDTO;
 import gabia.logConsumer.dto.WebhookMessage;
 import gabia.logConsumer.entity.Enum.WebhookEndpoint;
+import gabia.logConsumer.util.WebhookMessageFactory;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -77,24 +78,7 @@ public class WebhookService {
     @KafkaListener(topics = "webhook", groupId = "webhook_consumer", containerFactory = "webhookListener")
     public void webhookConsumer(WebhookDTO.Request request) throws IOException {
 
-        WebhookMessage webhookMessage = null;
-
-        if (request.getEndpoint().equals(WebhookEndpoint.SLACK)) {
-            webhookMessage = new SlackDTO();
-            webhookMessage.setText(request.getText());
-
-            Attachment attachment = Attachment.builder()
-                .title("Cron Status")
-                .text(request.getText())
-                .authorName("Cron Monitoring Server")
-                .build();
-
-            ((SlackDTO) webhookMessage).addAttachment(attachment);
-        } else if (request.getEndpoint().equals(WebhookEndpoint.HIWORKS)) {
-
-            webhookMessage = new HiworksDTO();
-            webhookMessage.setText(request.getText());
-        }
+        WebhookMessage webhookMessage = WebhookMessageFactory.getMessage(request);
 
         webhookBusiness.sendMessage(request, webhookMessage);
         log.info("Send message to {} in {}", request.getUrl(), request.getEndpoint().toString());
